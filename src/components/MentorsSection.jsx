b348cf2468b03994703629507ddcdaafcda939a0
avacoder <avazbeknematov01@gmail.com>
2026-09-22 11:54:10 +0500

    fix

--- src/components/MentorsSection.jsx
+++ src/components/MentorsSection.jsx
@@ -37,9 +37,7 @@ const MentorsSection = ({ variant }) => {
     const heading = isOnline
 		? splitHeading(st('education_title5'), t("components.mentorsSection.sohasida_tajribali_mutaxassislar_bilan"), t('components.mentorsSection.organing', "o'rganing!"))
 		: { title: t("components.mentorsSection.sohasida_tajribali_mutaxassislar_bilan"), highlight: t('components.mentorsSection.organing', "o'rganing!") }
-    const subtitleFallback = isOnline ?
-        splitHeading(st('education_description5'), t("components.mentorsSection.sohasida_tajribali_mutaxassislar_bilan"), t('components.mentorsSection.organing', "o'rganing!")):
-        (
+    const subtitleFallback = (
 		<>{t("components.mentorsSection.bizning_platforma_orqali_siz")}{' '}
 			<br className='hidden sm:block' />{t("components.mentorsSection.mahalliy_va_xorijiy_mutaxassislar")}</>
 	)
